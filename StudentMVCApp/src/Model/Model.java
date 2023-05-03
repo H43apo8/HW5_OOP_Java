afe4ba777c67da00e81b726a1b0ecf4ec3378626
H43apo8 <H43apo8@yandex.ru>
2023-05-03 12:27:46 +0300

    create  class Model

--- StudentMVCApp/src/Model/Model.java
+++ StudentMVCApp/src/Model/Model.java
@@ -1,18 +1,79 @@
 package Model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
-import Controller.iGetModel;
-
+// Класс Model, который реализует интерфейс iGetModel
 public class Model implements iGetModel {
-    private List<Student> students;
+    // Хранилище студентов в виде HashMap
+    private HashMap<Long, Student> students;
 
-    public Model(List<Student> students) {
-        this.students = students;
+    // Конструктор класса, инициализирующий хранилище
+    public Model() {
+        students = new HashMap<Long, Student>();
     }
 
-    public List<Student> getAllStudent()
-    {
-        return students;
+    // Метод добавления студента в хранилище
+    public void addStudent(Student student) {
+        students.put(student.getId(), student);
+    }
+
+    // Метод удаления студента из хранилища по ID
+    public void removeStudent(long id) {
+        students.remove(id);
+    }
+
+    // Метод получения студента из хранилища по ID
+    public Student getStudent(long id) {
+        return students.get(id);
+    }
+
+    // Метод получения списка всех студентов из хранилища
+    public ArrayList<Student> getAllStudents() {
+        return new ArrayList<Student>(students.values());
+    }
+}
+
+// Интерфейс iGetModel, определяющий методы работы с моделью
+public interface iGetModel {
+    // Метод добавления студента в модель
+    public void addStudent(Student student);
+    // Метод удаления студента из модели по ID
+    public void removeStudent(long id);
+    // Метод получения студента из модели по ID
+    public Student getStudent(long id);
+    // Метод получения списка всех студентов из модели
+    public ArrayList<Student> getAllStudents();
+}
+
+// Класс контроллера, который использует класс Model для работы с данными
+public class Controller {
+    // Экземпляр класса Model
+    private Model model;
+
+    // Конструктор класса контроллера, инициализирующий экземпляр класса Model
+    public Controller() {
+        model = new Model();
+    }
+
+    // Метод добавления студента в модель через класс Model
+    public void addStudent(Student student) {
+        model.addStudent(student);
+    }
+
+    // Метод удаления студента из модели по ID через класс Model
+    public void removeStudent(long id) {
+        model.removeStudent(id);
+    }
+
+    // Метод получения студента из модели по ID через класс Model
+    public Student getStudent(long id) {
+        return model.getStudent(id);
+    }
+
+    // Метод получения списка всех студентов из модели через класс Model
+    public ArrayList<Student> getAllStudents() {
+        return model.getAllStudents();
     }
 }
